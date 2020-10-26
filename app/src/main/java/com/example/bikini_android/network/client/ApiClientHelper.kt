903206ebab8f21eb6679a24f16ea3bad4778feea
ChoMk <cmk5432@naver.com>
2020-10-27 01:20:24 +0900

    Implement #2 helper 로직 수정 - service를 통하여 create 할 수 있는 로직 추가 - apiClient가 retrofit 상태가 되도록 factory 로직 수정

--- app/src/main/java/com/example/bikini_android/network/client/ApiClientHelper.kt
+++ app/src/main/java/com/example/bikini_android/network/client/ApiClientHelper.kt
@@ -8,11 +8,18 @@
 package com.example.bikini_android.network.client
 
 import com.example.bikini_android.network.NetworkConstants
+import retrofit2.Retrofit
+import kotlin.reflect.KClass
 
 /**
  * @author MyeongKi
  */
 
 object ApiClientHelper {
-    val serviceApiClient = ServiceApiFactory.getApiClient(NetworkConstants.BASE_URL)
+    private val apiClient: Retrofit = ServiceApiFactory.getApiClient(NetworkConstants.BASE_URL)
+
+    @JvmStatic
+    fun <T : Any> createApiByService(clazz: KClass<T>): T {
+        return apiClient.create(clazz.java)
+    }
 }
--- app/src/main/java/com/example/bikini_android/network/client/ServiceApiFactory.kt
+++ app/src/main/java/com/example/bikini_android/network/client/ServiceApiFactory.kt
@@ -8,17 +8,22 @@
 package com.example.bikini_android.network.client
 
 import com.example.bikini_android.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 
 /**
  * @author MyeongKi
  */
 
 object ServiceApiFactory {
-    fun getApiClient(baseUrl: String): RestApiClient {
-        return if (BuildConfig.DEBUG) {
+    fun getApiClient(baseUrl: String): Retrofit {
+        val clientBuilder = if (BuildConfig.DEBUG) {
             BikiniDebugApiClient(baseUrl)
         } else {
             BikiniServiceApiClient(baseUrl)
         }
+        val okHttpBuilder = OkHttpClient.Builder()
+        val retrofitBuilder = clientBuilder.builder(okHttpBuilder)
+        return clientBuilder.build(retrofitBuilder)
     }
 }
