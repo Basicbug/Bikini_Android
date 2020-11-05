dd98e30107988fae29d1ebdd7962325a28ed95fd
ChoMk <cmk5432@naver.com>
2020-11-05 22:54:22 +0900

    #5 item view model 추가

---
+++ app/src/main/java/com/example/bikini_android/ui/common/BindableLayout.kt
@@ -0,0 +1,19 @@
+/*
+ * BindableItem.kt 2020. 11. 5
+ *
+ * Copyright 2020 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.example.bikini_android.ui.common
+
+import androidx.annotation.LayoutRes
+
+/**
+ * @author MyeongKi
+ */
+
+interface BindableLayout {
+    @LayoutRes
+    fun getLayoutRes(): Int
+}
