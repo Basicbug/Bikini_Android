562b89a9ae798ec99e649c732275092cbfac6fd5
bsgreentea <bsgreentea1@gmail.com>
2020-12-04 22:27:30 +0900

    Refactor #9 Feed 구성 수정
    - Feed에 대한 피드백 반영

--- app/src/main/java/com/example/bikini_android/repository/feed/Feed.kt
+++ app/src/main/java/com/example/bikini_android/repository/feed/Feed.kt
@@ -3,9 +3,13 @@ package com.example.bikini_android.repository.feed
 /**
  * @author bsgreentea
  */
+@Suppress("unused")
 data class Feed(
     var feedNumOfUser: Int = 0,
     var userId: String = "sampleUserId",
     var content: String = "sampleContent",
-    var imageUri: String = "sampleImageUri"
+    var imageUri: String = "sampleImageUri",
+    var profileImageUri: String = "sampleProfile",
+    var longitude: String = "xx.xxxx",
+    var latitude: String = "yy.yyyy"
 )
