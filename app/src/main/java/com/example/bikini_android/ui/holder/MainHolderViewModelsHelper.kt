8636b134c295658c3b7222a9ac0a2c756e1adfae
ChoMk <cmk5432@naver.com>
2020-12-07 19:28:20 +0900

    Refactor #46 ViewModelsHelper 추상층 추가

--- app/src/main/java/com/example/bikini_android/ui/holder/MainHolderViewModelsHelper.kt
+++ app/src/main/java/com/example/bikini_android/ui/holder/MainHolderViewModelsHelper.kt
@@ -11,22 +11,23 @@ package com.example.bikini_android.ui.holder
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import com.example.bikini_android.ui.base.BaseViewModel
+import com.example.bikini_android.ui.common.ViewModelsHelper
 import com.example.bikini_android.ui.feeds.FeedsViewModel
 
 /**
  * @author MyeongKi
  */
 
-object MainHolderViewModelsHelper {
+object MainHolderViewModelsHelper : ViewModelsHelper {
     @Suppress("UNCHECKED_CAST")
-    fun getViewModels(owner: ViewModelStoreOwner): List<BaseViewModel> {
+    override fun getViewModels(owner: ViewModelStoreOwner): List<BaseViewModel> {
         return listOf(
             ViewModelProvider(
                 owner
             )[FeedsViewModel::class.java]
         )
     }
-    fun saveState(viewModels:List<BaseViewModel>){
+    override fun saveState(viewModels:List<BaseViewModel>){
         for(viewModel in viewModels){
             viewModel.saveState()
         }
