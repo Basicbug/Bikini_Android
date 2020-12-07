a87555361198d50bb061c1b22b21f50bc53c644b
ChoMk <cmk5432@naver.com>
2020-12-07 13:10:21 +0900

    N/A 코드리뷰 반영

--- app/src/main/java/com/example/bikini_android/repository/feed/Feed.kt
+++ app/src/main/java/com/example/bikini_android/repository/feed/Feed.kt
@@ -1,13 +1,14 @@
 package com.example.bikini_android.repository.feed
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author bsgreentea
  */
 @Suppress("unused")
+@Parcelize
 data class Feed(
     var feedNumOfUser: Int = 0,
     var userId: String = "sampleUserId",
@@ -16,38 +17,4 @@ data class Feed(
     var profileImageUrl: String = "sampleProfile",
     val position: LatLng?,
     var countOfGroupFeed: Int = 1
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readParcelable(LatLng::class.java.classLoader),
-        parcel.readInt()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(feedNumOfUser)
-        parcel.writeString(userId)
-        parcel.writeString(content)
-        parcel.writeString(imageUrl)
-        parcel.writeString(profileImageUrl)
-        parcel.writeParcelable(position, flags)
-        parcel.writeInt(countOfGroupFeed)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Feed> {
-        override fun createFromParcel(parcel: Parcel): Feed {
-            return Feed(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Feed?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
