c470c60c7320df1aa9ac1ddfa41299ac61f9ff30
bsgreentea <bsgreentea1@gmail.com>
2020-12-16 22:41:51 +0900

    Modify #44 피드백 반영 - 불필요 키워드 제거

--- app/src/main/java/com/example/bikini_android/ui/feeds/adapter/FeedLinearItemViewModel.kt
+++ app/src/main/java/com/example/bikini_android/ui/feeds/adapter/FeedLinearItemViewModel.kt
@@ -8,7 +8,7 @@ import java.util.*
 /**
  * @author bsgreentea
  */
-open class FeedLinearItemViewModel : FeedItemViewModel() {
+class FeedLinearItemViewModel : FeedItemViewModel() {
 
     @get: Bindable
     var content = ""
