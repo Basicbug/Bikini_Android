1362e3ef82876e5520071b776886055cf468528a
bsgreentea <bsgreentea1@gmail.com>
2021-02-15 23:08:42 +0900

    no message

--- app/src/main/java/com/example/bikini_android/manager/login/NaverLoginFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bikini_android.manager.login
-
-/**
- * @author bsgreentea
- */
-class NaverLoginFactory : LoginFactory() {
-
-    override fun makeLoginManager(): LoginManager = NaverLoginManager()
-}
