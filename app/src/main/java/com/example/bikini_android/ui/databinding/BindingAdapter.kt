abda64262dcf54bacb46a8b96390e43e385e8300
bsgreentea <bsgreentea1@gmail.com>
2021-03-05 22:44:04 +0900

    no message

--- app/src/main/java/com/example/bikini_android/ui/databinding/BindingAdapter.kt
+++ app/src/main/java/com/example/bikini_android/ui/databinding/BindingAdapter.kt
@@ -2,7 +2,6 @@ package com.example.bikini_android.ui.databinding
 
 import android.view.View
 import androidx.databinding.BindingAdapter
-import com.example.bikini_android.manager.login.NaverOAuthLoginHandler
 import com.nhn.android.naverlogin.OAuthLoginHandler
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton
 
