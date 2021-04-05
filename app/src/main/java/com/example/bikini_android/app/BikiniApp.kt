c41dbcd741b8bd2c6585c3fca3b8c0ff70776bf3
bsgreentea <bsgreentea1@gmail.com>
2021-04-05 21:58:44 +0900

    #126 flipper 적용

--- app/src/main/java/com/example/bikini_android/app/BikiniApp.kt
+++ app/src/main/java/com/example/bikini_android/app/BikiniApp.kt
@@ -9,6 +9,13 @@ package com.example.bikini_android.app
 
 import android.app.Application
 import android.content.Context
+import com.example.bikini_android.BuildConfig
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 
 /**
  * @author MyeongKi
@@ -19,10 +26,26 @@ class BikiniApp : Application() {
         instance = this
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.apply {
+                addPlugin(InspectorFlipperPlugin(this@BikiniApp, DescriptorMapping.withDefaults()))
+                addPlugin(networkFlipperPlugin)
+            }
+            client.start()
+        }
+    }
+
     companion object {
         private var instance: BikiniApp? = null
         fun applicationContext(): Context {
             return instance!!.applicationContext
         }
+
+        val networkFlipperPlugin = NetworkFlipperPlugin()
     }
 }
--- app/src/main/java/com/example/bikini_android/network/client/RestApiClient.kt
+++ app/src/main/java/com/example/bikini_android/network/client/RestApiClient.kt
@@ -8,8 +8,10 @@
 package com.example.bikini_android.network.client
 
 import com.example.bikini_android.app.AppResources
+import com.example.bikini_android.app.BikiniApp
 import com.example.bikini_android.network.HostSelectionInterceptor
 import com.example.bikini_android.network.ResponseCacheFactory
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -27,6 +29,7 @@ abstract class RestApiClient(private val requestUrl: String) : ApiClient {
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(15, TimeUnit.SECONDS)
             .addInterceptor(HostSelectionInterceptor())
+            .addInterceptor(FlipperOkhttpInterceptor(BikiniApp.networkFlipperPlugin))
             .cache(ResponseCacheFactory().createCache(AppResources.getContext()))
         return Retrofit.Builder().client(builder.build())
     }
