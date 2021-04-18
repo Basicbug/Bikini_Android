ae303864cd41662686bf404990703e377e002acb
ChoMk <cmk5432@naver.com>
2021-04-18 23:05:42 +0900

    Fix #91 feed 모델 변경에 의한 imageUrl 로직 수정

---
+++ app/src/main/java/com/example/bikini_android/util/string/StringConstants.kt
@@ -0,0 +1,13 @@
+/*
+ * StringConstants.kt 2021. 4. 18
+ *
+ * Copyright 2021 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.example.bikini_android.util.string
+
+/**
+ * @author MyeongKi
+ */
+const val EMPTY_STRING = ""
