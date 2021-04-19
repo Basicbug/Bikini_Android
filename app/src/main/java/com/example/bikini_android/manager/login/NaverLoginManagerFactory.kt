2d6f6bbb43fef5b4d2a1c81c795421893b07f543
bsgreentea <bsgreentea1@gmail.com>
2021-04-19 21:31:17 +0900

    Refactor #57 패키지 이동으로 인한 파일 제거

--- app/src/main/java/com/example/bikini_android/manager/login/NaverLoginManagerFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bikini_android.manager.login
-
-/**
- * @author bsgreentea
- */
-class NaverLoginManagerFactory : LoginManagerFactory() {
-
-    override fun makeLoginManager(): LoginManager = NaverLoginManager.instance
-}
