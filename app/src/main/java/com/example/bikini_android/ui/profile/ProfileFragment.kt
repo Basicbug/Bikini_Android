769593972ac5a6ae30c890a65fc9137f75e83c46
bsgreentea <bsgreentea1@gmail.com>
2021-05-26 23:47:30 +0900

    Revert "#150 피드백 반영"
    This reverts commit 1a0ef2a49f671526efe78a466f9e8996ba1ffe0c.

--- app/src/main/java/com/example/bikini_android/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/bikini_android/ui/profile/ProfileFragment.kt
@@ -90,7 +90,7 @@ class ProfileFragment : BaseFragment() {
         return when (item.itemId) {
             R.id.profile_detail_menu -> {
                 getNavigationHelper()?.navigateToProfileDetail()
-                true
+                false
             }
             else -> super.onOptionsItemSelected(item)
         }
