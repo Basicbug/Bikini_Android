c3188572eec950a1ab728a9d470f1b6deee768fa
ChoMk <cmk5432@naver.com>
2021-07-12 22:17:02 +0900

    #164 코드정리

--- app/src/main/java/com/example/bikini_android/ui/login/LoginRepository.kt
+++ app/src/main/java/com/example/bikini_android/ui/login/LoginRepository.kt
@@ -2,6 +2,7 @@ package com.example.bikini_android.ui.login
 
 import com.example.bikini_android.network.client.ApiClientHelper
 import com.example.bikini_android.network.request.service.AuthService
+import com.example.bikini_android.network.response.LoginResponse
 import com.example.bikini_android.util.error.ErrorToastHelper
 import com.example.bikini_android.util.logging.Logger
 import io.reactivex.Single
@@ -18,17 +19,15 @@ class LoginRepository {
         }
     }
 
-    fun sendTokenToServer(accessToken: String): Single<LoginJwtResult> {
+    fun sendTokenToServer(accessToken: String): Single<LoginResponse.Result?> {
         return ApiClientHelper
             .createMainApiByService(AuthService::class)
             .loginNaver(accessToken = accessToken)
             .subscribeOn(Schedulers.io())
-            .map { LoginJwtResult(it.result?.token) }
+            .map { it.result }
             .onErrorReturn { throwable ->
                 ErrorToastHelper.unknownError(logger, throwable)
                 null
             }
     }
-
-    data class LoginJwtResult(val jwt: String?)
 }
