baa8ef66ad4259df3e5e3b57132ac2f7bcd308d0
bsgreentea <bsgreentea1@gmail.com>
2021-08-31 02:15:38 +0900

    Refactor #153 사용자 정보 관련 response 객체 수정

--- app/src/main/java/com/example/bikini_android/network/response/MyInfoReponse.kt
+++ app/src/main/java/com/example/bikini_android/network/response/MyInfoReponse.kt
@@ -1,8 +1,14 @@
 package com.example.bikini_android.network.response
 
 import com.example.bikini_android.repository.account.UserInfo
+import com.google.gson.annotations.SerializedName
 
 /**
  * @author bsgreentea
  */
-class MyInfoReponse : JsonResponseWrapper<UserInfo>()
+class MyInfoReponse : JsonResponseWrapper<MyInfoReponse.Result>() {
+    data class Result(
+        @SerializedName("userInfo")
+        val userInfo: UserInfo
+    )
+}
