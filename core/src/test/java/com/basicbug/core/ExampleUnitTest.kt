89ba065b02cc9074beae48e5ee59c506deab6953
ChoMk <cmk5432@naver.com>
2021-11-24 21:43:38 +0900

    #189 init core package

---
+++ core/src/test/java/com/basicbug/core/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+/*
+ * ExampleUnitTest.kt 2021. 11. 24
+ *
+ * Copyright 2021 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.basicbug.core
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
